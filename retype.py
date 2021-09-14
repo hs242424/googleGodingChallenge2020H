6893b4ac1e980133f023673db59fbebf10f7ddf9
Imran Iftikar <imran.aidan.ai@gmail.com>
2021-09-14 13:06:02 -0400

    Update retype.py

--- retype.py
+++ retype.py
@@ -9,6 +9,6 @@ for i in range(cases):
     ans2 = (k-s)+(n-s+1)
     
     if ans1 <= ans2:
-        print(f"Case #{cases+1}: {ans1+ktime} ")
+        print("Case #{}: {}".format(i+1, ans1))
     else:
-        print(f"Case #{cases+1}: {ans2+ktime}")
+        print("Case #{}: {}".format(i+1, ans2))
